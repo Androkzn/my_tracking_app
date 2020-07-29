9089149068c7afadfbefe1ab137b1df5070d7f15
Andrei Tekhtelev <a.tekhtelev@gmail.com>
2020-07-29 14:00:31 -0700

    improves picker

--- my_tracking_app/ProfileViewController.swift
+++ my_tracking_app/ProfileViewController.swift
@@ -21,23 +21,30 @@ class ProfileViewController: UIViewController {
 
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     @IBAction func ageTextFieldBeginEditing(_ sender: UITextField) {
         print("ageStartEditing")
-        let ageRows:[Int] = Array(1...150)
+        let ageRows:[Int] = Array(1...100)
         textFieldPicker(sender, rows: ageRows,  title: "Select your age")
+        view.endEditing(true)
     }
 
     @IBAction func genderTextFieldBeginEditing(_ sender: UITextField) {
         print("genderStartEditing")
         let genderRows:[String] = ["Male", "Female", "Other"]
         textFieldPicker(sender, rows: genderRows,  title: "Select your gender")
+        view.endEditing(true)
         
     }
     
     @IBAction func weightTextFieldBeginEditing(_ sender: UITextField) {
         print("weightStartEditing")
-        let ageRows:[Int] = Array(1...300)
+        let ageRows:[Int] = Array(1...200)
         textFieldPicker(sender, rows: ageRows,  title: "Select your weight")
+        view.endEditing(true)
          
     }
     
@@ -45,58 +52,49 @@ class ProfileViewController: UIViewController {
         print("heightStartEditing")
         let ageRows:[Int] = Array(1...250)
         textFieldPicker(sender, rows: ageRows,  title: "Select your height")
+        view.endEditing(true)
         
     }
     
     func textFieldPicker(_ sender: UITextField, rows: [Any], title: String) {
-        ActionSheetStringPicker.show(withTitle: title,
+        let picker = ActionSheetStringPicker(title: title,
                                      rows: rows,
-                                     initialSelection: 1,
+                                     initialSelection: rows.count/3,
                                      doneBlock: { picker, value, index in
                                         //printings
                                         print("picker = \(String(describing: picker))")
                                         print("value = \(value)")
                                         print("index = \(String(describing: index))")
                                         sender.text = "\(index!)"
-                                        self.view.endEditing(true)
                                         return
                                      },
                                      cancel: { picker in
                                         return
                                      },
                                      origin: sender)
+     
+        // customize appearance of the picker
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.alignment = .center
+        picker!.pickerTextAttributes = [NSAttributedString.Key.paragraphStyle: paragraph, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20.0)]
+        picker!.setTextColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        picker!.pickerBackgroundColor = #colorLiteral(red: 1, green: 0.8085083365, blue: 0.4892358184, alpha: 1)
+        picker!.toolbarBackgroundColor = #colorLiteral(red: 0.3249011148, green: 0.7254286438, blue: 0.9069467254, alpha: 0.8043396832)
+        //right button
+        let okButton = UIButton()
+        okButton.setTitle("OK", for: .normal)
+        okButton.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        let customDoneButton = UIBarButtonItem.init(customView: okButton)
+        picker!.setDoneButton(customDoneButton)
+        //left button
+        let cancelButton = UIButton()
+        cancelButton.setTitle("CANCEL", for: .normal)
+        cancelButton.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        let customCancelButton = UIBarButtonItem.init(customView: cancelButton)
+        picker!.setCancelButton(customCancelButton)
         
-    }
-    
-    
-    
-    
+        picker!.show()      }
     
 }
 
-extension ProfileViewController: UITextFieldDelegate {
-    func textFieldShouldReturn (_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        print("textFieldSentData")
-        if textField == ageTextField {
-            print("age")
-            let age = textField.text ?? ""
-        }
-        if textField == genderTextField {
-            print("gender")
-            let gender = textField.text ?? ""
-        }
-        if textField == weightTextField {
-            print("weight")
-            let weight = textField.text ?? ""
-    
-        }
-        if textField == heightTextField {
-            print("height")
-            let height = textField.text ?? ""
-
-        }
-        return true
-    }
-}
     
